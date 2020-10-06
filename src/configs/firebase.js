0653b75d313fb7a425894e1c6dccf448a298f72c
ChrissGS <stefan_gruescu@yahoo.com>
2020-10-06 15:58:18 +0300

    make it better

---
+++ src/configs/firebase.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+	apiKey: 'AIzaSyBb9ZxcutvWNws3-S5jKtAnrCgvotHvJLA',
+	authDomain: 'abibas-shop.firebaseapp.com',
+	databaseURL: 'https://abibas-shop.firebaseio.com',
+	projectId: 'abibas-shop',
+	storageBucket: 'abibas-shop.appspot.com',
+	messagingSenderId: '937394197189',
+	appId: '1:937394197189:web:d7671c69d043436bba96ac'
+};
+
+export default firebaseConfig;
