f9099984fc45062fa49367f534db1910ae79ac6a
ChrissGS <stefan_gruescu@yahoo.com>
2020-10-07 14:26:14 +0300

    full firebase security fix

--- src/configs/firebase.js
+++ src/configs/firebase.js
@@ -1,13 +1,12 @@
-import React from 'react';
 require('dotenv');
 
 const firebaseConfig = {
 	apiKey: process.env.REACT_APP_API_KEY,
-	authDomain: 'abibas-shop.firebaseapp.com',
-	databaseURL: 'https://abibas-shop.firebaseio.com',
-	projectId: 'abibas-shop',
-	storageBucket: 'abibas-shop.appspot.com',
-	messagingSenderId: '937394197189',
-	appId: '1:937394197189:web:d7671c69d043436bba96ac'
+	authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+	databaseURL: process.env.REACT_APP_DATABASE_URL,
+	projectId: process.env.REACT_APP_PROJECT_ID,
+	storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+	messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+	appId: process.env.REACT_APP_APP_ID
 };
 export default firebaseConfig;
